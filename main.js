39f98635fc3f0b9b3e81f13ad1d3aca0bfbe2706
Atasha Potter <Ms.APotter@gmail.com>
2019-04-16 17:20:40 -0400

    character images added probably need to get rid of bootstrap as pics dont fit

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
-phrases = ["winter is coming", "valar morghulis"];
+// Array of phrases chosen by creator
+phrases = ["winter is coming", "valar morghulis"];      
     console.log(phrases)
-answerPhrases = phrases[0].split("");
+
+// Splits each phrase (index) into separate string letters (to be looped thru later)
+answerPhrases = phrases[0].split("");                   
     console.log(answerPhrases);
     console.log(answerPhrases[0][0]);
 
-for (var i =0; i < answerPhrases.length; i++) {
-    document.querySelector('#guess-what-phrase');
 
+wordToGuess = document.querySelector('#guess-what-phrase');
+    console.log(wordToGuess);
+
+// Loops through a phrase and creates as many horizontal lines as there are letters in the phrase
+for (var i =0; i < answerPhrases.length; i++) {         
+    let letterToGuess = document.createElement('hr')                
+        letterToGuess.style.borderColor = "white"; 
+        letterToGuess.style.width = "25px";
+        document.querySelector('#guess-what-phrase').appendChild(letterToGuess)
+
+}
+
+let selectAllLetters = document.querySelectorAll('.letter')
+console.log(selectAllLetters)
+let ugh = document.getElementsByClassName('letter')
+console.log(ugh)
+let bleh = document.getElementsByTagName('img')
+console.log(bleh)
+
+let userClicksOnLetter = selectAllLetters.addEventListener('click', someFunction)
+
+
+function someFunction() {
+    console.log("works")
+
+    for (var i = 0; i < answerPhrases.length; i++) {
+        if (selectAllLetters[0].dataset.letter  == answerPhrases[i]) {  // OR selectAllLetters.innerText
+            document.querySelectorAll(`[data-letter=${i}]`)[i].innerHTML = selectAllLetters[0].dataset.letter
+    }
+    
+    }
+  
+    
 }
