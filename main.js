d5740c7d4be6db4044c4453cd39f74e304764c76
Atasha Potter <Ms.APotter@gmail.com>
2019-04-16 14:33:06 -0400

    made test phrases array and made individual letters to loop thru later

---
+++ main.js
@@ -0,0 +1,10 @@
+phrases = ["winter is coming", "valar morghulis"];
+    console.log(phrases)
+answerPhrases = phrases[0].split("");
+    console.log(answerPhrases);
+    console.log(answerPhrases[0][0]);
+
+for (var i =0; i < answerPhrases.length; i++) {
+    document.querySelector('#guess-what-phrase');
+
+}
