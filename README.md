21ca98e33c68edec66770ce378c2d5772d74d965
Atasha Potter <Ms.APotter@gmail.com>
2019-04-16 21:07:09 -0400

    looped thru test phrase and got underlines and full phrase to show above the AZ keys

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Game-of-Thrones-Hangman
+
+
+Hurdle #1:  Bootstrap doesn't play well with others. I tried using Bootstrap for the styling of the buttons, using cards and putting them in a container, but I couldn't adjust the spacing to use the full horizontal space. I finally had to abandon bootstrap and build it with HTML and CSS.
--- main.js
+++ main.js
@@ -1,44 +1,56 @@
 // Array of phrases chosen by creator
-phrases = ["winter is coming", "valar morghulis"];      
-    console.log(phrases)
+arrayofPhrases = ["winter is coming", "valar morghulis"];      
+    console.log(arrayofPhrases)
 
 // Splits each phrase (index) into separate string letters (to be looped thru later)
-answerPhrases = phrases[0].split("");                   
-    console.log(answerPhrases);
-    console.log(answerPhrases[0][0]);
+phrases = arrayofPhrases[0].split("");                   
+    console.log(phrases);
+    console.log(phrases[0][0]);
 
 
-wordToGuess = document.querySelector('#guess-what-phrase');
+wordToGuess = document.querySelector('#phrase');
     console.log(wordToGuess);
 
-// Loops through a phrase and creates as many horizontal lines as there are letters in the phrase
-for (var i =0; i < answerPhrases.length; i++) {         
-    let letterToGuess = document.createElement('hr')                
-        letterToGuess.style.borderColor = "white"; 
-        letterToGuess.style.width = "25px";
-        document.querySelector('#guess-what-phrase').appendChild(letterToGuess)
+// Loops through a phrase and create as many divs with the corresponding letter, 
+// as there are letters in the phrase
+for (var i =0; i < phrases.length; i++) {         
+    let divForLetter = document.createElement('div')
+    divForLetter.className = "letter";
+        document.querySelector('#phrase').appendChild(divForLetter)
+    // let para = document.createElement('p')
+    //     divForLetter.appendChild(para);
+    let letters = document.createTextNode(phrases[i])
+    // let letters = para.textContent = phrases[i]
+    console.log(letters)
+        divForLetter.appendChild(letters)
+        console.log(divForLetter)
+    let lineAsLetterPlaceholder = document.createElement('hr')
+    lineAsLetterPlaceholder.className = "line"
+    divForLetter.appendChild(lineAsLetterPlaceholder)
+        // document.querySelector('para').classNmae = "style"             
+        // letters.appendChild(divForLetter);
+        // letterCharacter.style. = "white"; 
+        // letterCharacter.style.width = "25px";
+        // document.querySelector('#guess-what-phrase').appendChild(letterToGuess)
 
 }
 
 let selectAllLetters = document.querySelectorAll('.letter')
 console.log(selectAllLetters)
-let ugh = document.getElementsByClassName('letter')
-console.log(ugh)
-let bleh = document.getElementsByTagName('img')
-console.log(bleh)
-
-let userClicksOnLetter = selectAllLetters.addEventListener('click', someFunction)
 
 
-function someFunction() {
-    console.log("works")
+// let userClicksOnLetter = selectAllLetters.addEventListener('click', someFunction)
 
-    for (var i = 0; i < answerPhrases.length; i++) {
-        if (selectAllLetters[0].dataset.letter  == answerPhrases[i]) {  // OR selectAllLetters.innerText
-            document.querySelectorAll(`[data-letter=${i}]`)[i].innerHTML = selectAllLetters[0].dataset.letter
-    }
+
+// function someFunction() {
+//     console.log("works")
+
+//     for (var i = 0; i < answerPhrases.length; i++) {
+//         if (selectAllLetters[0].dataset.letter  == answerPhrases[i]) {  // OR selectAllLetters.innerText
+//             document.querySelectorAll(`[data-letter=${i}]`)[i].innerHTML = selectAllLetters[0].dataset.letter
+//     }
     
-    }
+//     }
   
     
-}
+// }
