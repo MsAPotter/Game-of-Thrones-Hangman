bb120e8155f01e7ef330d6fab9381923e5449719
Atasha Potter <Ms.APotter@gmail.com>
2019-04-16 21:39:54 -0400

    Added event listener to listen for click on buttons

--- main.js
+++ main.js
@@ -11,46 +11,63 @@ phrases = arrayofPhrases[0].split("");
 wordToGuess = document.querySelector('#phrase');
     console.log(wordToGuess);
 
-// Loops through a phrase and create as many divs with the corresponding letter, 
-// as there are letters in the phrase
+// Loops through a phrase and creates as many divs as there are letters in the phrase
+// Adds a class to div in order to style as desired
+// Adds letters of the phrase to the divs
+// Also creates underlines (hr) under each div element (would not append to letters)
 for (var i =0; i < phrases.length; i++) {         
     let divForLetter = document.createElement('div')
-    divForLetter.className = "letter";
+        divForLetter.className = "letter";
         document.querySelector('#phrase').appendChild(divForLetter)
-    // let para = document.createElement('p')
-    //     divForLetter.appendChild(para);
+
     let letters = document.createTextNode(phrases[i])
-    // let letters = para.textContent = phrases[i]
-    console.log(letters)
+        console.log(letters)
         divForLetter.appendChild(letters)
         console.log(divForLetter)
-    let lineAsLetterPlaceholder = document.createElement('hr')
-    lineAsLetterPlaceholder.className = "line"
-    divForLetter.appendChild(lineAsLetterPlaceholder)
-        // document.querySelector('para').classNmae = "style"             
-        // letters.appendChild(divForLetter);
-        // letterCharacter.style. = "white"; 
-        // letterCharacter.style.width = "25px";
-        // document.querySelector('#guess-what-phrase').appendChild(letterToGuess)
 
+    let lineAsLetterPlaceholder = document.createElement('hr')
+        lineAsLetterPlaceholder.className = "line"
+        divForLetter.appendChild(lineAsLetterPlaceholder)
 }
 
-let selectAllLetters = document.querySelectorAll('.letter')
+
+//  Adding event listener to listen for click on buttons
+
+// Reference: https://stackoverflow.com/questions/40956717/how-to-addeventlistener-to-multiple-elements-in-a-single-line
+
+// ==> ARE WE NOT ABLE TO LISTEN ON MULTIPLE LEMENTS AT ONCE?? (ie could not get the quearySelectorAll to work with a click function, until I added a forEach function)
+
+let selectAllLetters = document.querySelectorAll('.button')
 console.log(selectAllLetters)
 
 
-// let userClicksOnLetter = selectAllLetters.addEventListener('click', someFunction)
+selectAllLetters.forEach(function(userClicksOnLetter) {
+    userClicksOnLetter.addEventListener("click", function() {
+        console.log("test");
+        //this function does stuff
+    });
+});
 
 
-// function someFunction() {
-//     console.log("works")
+// On start, shuffle array of phrases
+// Hide phrase at START
+// visibility: hidden;
 
-//     for (var i = 0; i < answerPhrases.length; i++) {
-//         if (selectAllLetters[0].dataset.letter  == answerPhrases[i]) {  // OR selectAllLetters.innerText
-//             document.querySelectorAll(`[data-letter=${i}]`)[i].innerHTML = selectAllLetters[0].dataset.letter
-//     }
+
+// let userClicksOnLetter = selectAllLetters.addEventListener("click", function() {
+//     console.log("test");
+// });
+    // for (var i = 0; i < phrases.length; i++) {
+    //     if (selectAllLetters[0].dataset.letter  == answerPhrases[i]) {  // OR selectAllLetters.innerText
+    //         document.querySelectorAll(`[data-letter=${i}]`)[i].innerHTML = selectAllLetters[0].dataset.letter
+    // }
     
-//     }
+    // }
   
-    
+
+// for (let i = 0; i < phrases.length; i++) {
+//     phrases[i].addEventListener('click', function(evt) {    
+//       evt.preventDefault();
+
+// })
 // }
