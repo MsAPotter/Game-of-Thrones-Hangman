c00f6b8409455eec3b81530cddf452f5a39fdb32
Atasha Potter <Ms.APotter@gmail.com>
2019-04-19 11:15:54 -0400

    used dataset in JS DOM to target the letters specifically

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 // ARRAY OF PHRASES CHOSEN BY CREATOR
-arrayofPhrases = ["winter is coming", "valar morghulis", "valar dohaeris", "i drink and i know things","you know nothing jon snow", "chaos is a ladder", "hold the door"];      
+arrayofPhrases = ["winter is coming", "valar morghulis", "valar dohaeris", "i drink and i know things", "you know nothing jon snow", "chaos is a ladder", "hold the door"];
 console.log(arrayofPhrases)
- 
+
 
 //  RANDOMIZE ARRAY OF PHRASES AT EACH START 
 //  Reference: https://codepen.io/GeeksRock/pen/JoZEVb?editors=0010
 //  Reference:  Game of War Lab
 
 function GetRandomPhrase() {
-        var j = Math.floor((Math.random() * 7));
-        randomPhrase = arrayofPhrases[j];
-        return randomPhrase;
+    var j = Math.floor((Math.random() * 7));
+    randomPhrase = arrayofPhrases[j];
+    return randomPhrase;
 }
 GetRandomPhrase()
 console.log(randomPhrase)
@@ -22,16 +22,16 @@ console.log(randomPhrase)
 var lastUsedPhrase;
 
 function NoTwoWordsTheSame() {
-	randomPhrase = GetRandomPhrase();
- //never use the same random word two times in a row
- if (randomPhrase[0] === lastUsedPhrase) {
-    //request a new random word
-    NoTwoWordsTheSame();
-  } else {
-    //track the last random word used
-    lastUsedPhrase = randomPhrase[0];
-    return lastUsedPhrase
-  }
+    randomPhrase = GetRandomPhrase();
+    //never use the same random word two times in a row
+    if (randomPhrase[0] === lastUsedPhrase) {
+        //request a new random word
+        NoTwoWordsTheSame();
+    } else {
+        //track the last random word used
+        lastUsedPhrase = randomPhrase[0];
+        return lastUsedPhrase
+    }
 }
 NoTwoWordsTheSame()
 console.log(randomPhrase)
@@ -41,10 +41,10 @@ console.log(randomPhrase)
 
 // Splits each phrase (index) into separate string letters (to be looped thru later)
 
-let lettersOfEachPhrase = randomPhrase.split(""); 
+let lettersOfEachPhrase = randomPhrase.split("");
 console.log(lettersOfEachPhrase);
 console.log(lettersOfEachPhrase.length)
-                                                                            
+
 
 // ADD IMAGE UNDER HEADER
 
@@ -81,32 +81,33 @@ imageTagForImage.setAttribute("src", arrayOfImages[0]);
 placeholderForLetter = [];
 let numOfCorrectGuesses = 0;
 
-for (var i =0; i < lettersOfEachPhrase.length; i++) {    
-    
+for (var i = 0; i < lettersOfEachPhrase.length; i++) {
+
     if (lettersOfEachPhrase[i] == " ") {
         numOfCorrectGuesses++;
-        console.log("The number of correct guesses is:" +numOfCorrectGuesses)
+        console.log("The number of correct guesses is:" + numOfCorrectGuesses)
     }
 
     let divForLetter = document.createElement('div')
-        divForLetter.className = "letter";
-        document.querySelector('#phrase').appendChild(divForLetter)
+    divForLetter.className = "letter";
+    document.querySelector('#phrase').appendChild(divForLetter)
 
     placeholderForLetter = document.createElement('span')       // NEEDED TO ADD A SPAN IOT HIDE LETTERS ==> IDEA FROM DANNY
-        placeholderForLetter.className = "hide-me";             // hides letters
-        divForLetter.appendChild(placeholderForLetter)
-        // console.log(placeholderForLetter)
+    placeholderForLetter.className = "hide-me";             // hides letters
+    placeholderForLetter.dataset.letterValue = lettersOfEachPhrase[i]
+    divForLetter.appendChild(placeholderForLetter)
+    // console.log(placeholderForLetter)
 
     letter = document.createTextNode(lettersOfEachPhrase[i])
-        // console.log(letter)
-        placeholderForLetter.appendChild(letter)
-        // console.log(divForLetter)
+    // console.log(letter)
+    placeholderForLetter.appendChild(letter)
+    // console.log(divForLetter)
 
     let lineAsLetterPlaceholder = document.createElement('hr')      // NEEDED TO APPEND HR TO DIV (REMOVE FROM SAME ELEMENT AS LETTER) IOT HIDE LETTERS
-        lineAsLetterPlaceholder.className = "line";
-        divForLetter.appendChild(lineAsLetterPlaceholder)
+    lineAsLetterPlaceholder.className = "line";
+    divForLetter.appendChild(lineAsLetterPlaceholder)
 }
- 
+
 
 
 //  Adding event listener to listen for click on buttons
@@ -135,66 +136,68 @@ console.log(allButtons)
 
 function playGame() {
 
-    allButtons.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    console.log(evt.target.id)
-    if (evt.target !== allButtons) {        //  ignores clicking on container of buttons
+    allButtons.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        // console.log(evt.target.id)
+        if (evt.target !== allButtons) {        //  ignores clicking on container of buttons
 
-            
 
-            for (var i =0; i < lettersOfEachPhrase.length; i++) { 
+            if (lettersOfEachPhrase.includes(evt.target.id)) {
+                console.log('includes', evt.target.id)
+                event.target.disabled = true;
+                event.target.style.opacity = 0.3;
 
-                if (evt.target.id == lettersOfEachPhrase[i]) {  
 
-                    document.getElementsByTagName('span')[i].removeAttribute("class");
+                let letters = document.querySelectorAll(`[data-letter-value='${evt.target.id}']`)
+                console.log(letters)
+                for (let i = 0; i < letters.length; i++) {
+                    letters[i].removeAttribute("class");
 
+
+                    numOfCorrectGuesses++;
+                    console.log("The new number of correct guesses is: " + numOfCorrectGuesses);
+
+                    if (numOfCorrectGuesses === lettersOfEachPhrase.length) {
+                        console.log("win");
+                        win()
+                    }
+                }
+
+
+
+
+
+            }
+            else {
+                console.log('no letter found', evt.target.id)
+                numOfWrongGuesses--;
+
+                if (numOfWrongGuesses <= 7) {
                     event.target.disabled = true;
                     event.target.style.opacity = 0.3;
 
-                    numOfCorrectGuesses++;
-                    console.log("The new number of correct guesses is: "+numOfCorrectGuesses);
+                    imageTagForImage.style.opacity = op;
+                    imageTagForImage.style.filter = 'alpha(opacity=" + op * 100 + ")';
+                    op += op * 0.025;
+                }
+                if (numOfWrongGuesses == 1) {
+                    console.log("Game over");
+                }
+            }
 
-                        if (numOfCorrectGuesses === lettersOfEachPhrase.length) {
-                            console.log("win");
-                            win()
-                        }
-                
 
-                }   else {
-                    
-                    if (evt.target.id !== lettersOfEachPhrase[i]) {
 
-                        if (numOfWrongGuesses <= 7) {
-                            event.target.disabled = true;
-                            event.target.style.opacity = 0.3;       
+            // } 
+            console.log("The number of wrong guesses is: " + numOfWrongGuesses)
 
-                            imageTagForImage.style.opacity = op;    
-                            imageTagForImage.style.filter = 'alpha(opacity=" + op * 100 + ")';
-                            op += op * 0.025;
-                        }
-                        if (numOfWrongGuesses == 1) {
-                            console.log("Game over");
-                            //     // display Game Over
-                            //     // play Video
-                            //     // display button asking "Try again?"
-                            //     // if button clicked, reset game (shuffle phrases array)
-                        }
-                    }
-  
-                        }
 
-            } 
-            numOfWrongGuesses--;
-            console.log("The number of wrong guesses is: "+numOfWrongGuesses)
-           
-                
         }
 
-})
+    })
 }
 playGame()
 
-     
+
 //  WIN FUNCTION ==> EXCHANGES IMAGE FOR GIF + TEXT + (NEED TO ADD 'BUTTON TO RESET GAME')
 
 function win() {
@@ -223,9 +226,9 @@ function win() {
 //     phraseSplit()
 // });
 
-   
 
-   
+
+
 
 
 
@@ -242,7 +245,7 @@ function win() {
 //     console.log(evt.target.id)
 
 //     if (evt.target !== containerOfButtons) {                  //  ignores clicking on container of buttons    
-           
+
 //         lettersOfEachPhrase.forEach(function(value){
 //             if (evt.target.id == lettersOfEachPhrase) {
 //                 console.log("bleh")
@@ -278,5 +281,5 @@ function win() {
 
 //            });
 //         }});
-        
+
 
